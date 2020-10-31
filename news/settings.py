0d7ef3835071769d363548d590987eb1cca23db1
vladikkuzn <vladikkuzn1@gmail.com>
2020-10-31 18:30:15 +0200

    Unnecessary imports removed

--- news/settings.py
+++ news/settings.py
@@ -12,8 +12,6 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 import os
-import dj_database_url
-import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -134,6 +132,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-django_heroku.settings(locals())
 
 STATIC_URL = '/static/'
